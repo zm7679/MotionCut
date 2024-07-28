631003b6cac038e1170f482325262f172e654584
zm7679 <zaidmomin518@gmail.com>
2024-07-28 21:03:16 +0530

    Create UserController.java

---
+++ Task 3/UserController.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class UserController {
+    private List<User> users;
+
+    public void registerUser(String username, String password) {
+        // Implement registration logic
+    }
+
+    public boolean authenticateUser(String username, String password) {
+        // Implement authentication logic
+        return true;
+    }
+}
