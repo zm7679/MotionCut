b764755f44750a638adf5674f11a7283268c2f58
zm7679 <zaidmomin518@gmail.com>
2024-07-28 21:02:05 +0530

    Create ExpenseView.java

---
+++ Task 3/ExpenseView.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class ExpenseView {
+    public void displayExpenses(List<Expense> expenses) {
+        // Implement logic to display expenses
+    }
+
+    public void displayCategorySummary(double total, String category) {
+        // Implement logic to display category-wise total
+    }
+}
